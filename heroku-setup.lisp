81b4ee9097825cc69e317f1709d6b2c0340af546
kjcjohnson@ymail.com <kjcjohnson@ymail.com>
2013-05-15 16:13:00 -0700

    Added REPL 8th Try

--- heroku-setup.lisp
+++ heroku-setup.lisp
@@ -4,6 +4,7 @@
 
 (load (merge-pathnames "kjcjohnson-site.asd" *build-dir*))
 
+(ql:quickload :ht-simple-ajax)
 (ql:quickload :kjcjohnson-site)
 
 ;;; Redefine / extend heroku-toplevel here if necessary.
